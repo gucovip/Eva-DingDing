b6dd02636db4853344f92c1512a3c62a853afc43
Cong <raiyeeeric@gmail.com>
2018-10-21 12:27:36 +0800

    fix:bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const schedule = require('node-schedule')
 const {dingText, dingLink} = require('./DingSdk.js')
 
-dingText('Eva自动打卡提醒已启动~，将在每天上午的8点30分提醒大家！')
+dingText('Eva自动打卡提醒已启动~，将在每天上午的8点45分提醒大家！')
 dingLink('更新提醒', '1、提醒规则修改，上午8点45分到上午8点55分每隔5分钟提醒一次；2、去除24小时报时，加入6小时一次的心跳；3、新增DingSdk。', 'https://github.com/raiyeeeric/Eva-DingDing', 'https://raw.githubusercontent.com/eva-org/eva-core/master/icon.png')
 
 schedule.scheduleJob('0 45 8 * * *', function () {
