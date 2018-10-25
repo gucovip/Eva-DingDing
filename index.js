144454eed0bc5e31e8d0438d7f7ea6bc4a31f3eb
cong <raiyeeeric@gmail.com>
2018-10-25 08:55:49 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const schedule = require('node-schedule')
 const {dingText, dingLink} = require('./DingSdk.js')
 
-dingText('Eva自动打卡提醒已启动~，将在每天上午的8点45分提醒大家！')
+// dingText('Eva自动打卡提醒已启动~，将在每天上午的8点45分提醒大家！')
 dingLink('更新提醒', '1、修改错别字；2、重构代码。', 'https://github.com/raiyeeeric/Eva-DingDing', 'https://raw.githubusercontent.com/eva-org/eva-core/master/icon.png')
 
-schedule.scheduleJob('0 45/50/55 8 * * *', function () {
+schedule.scheduleJob('0 45,50,55 8 * * *', function () {
     const exportDay = [0, 6]
     const todayOfWeek = new Date().getDay()
     if (exportDay.includes(todayOfWeek)) return undefined
